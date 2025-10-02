4ddd25066e6daa33e2dbdb689490bd0b5199479b
JitChowDhury <jitchowdhury8717@gmail.com>
2025-10-02 21:47:19 +0530

    Creating The Food

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,53 @@
 Color green = { 173,204,96,255 };
 Color darkGreen = { 43,51,24,255 };
 
+int cellSize = 30;
+int cellCount = 25;
+
+
+class Food
+{
+public:
+	Vector2 position;
+	Texture2D texture;
+
+	Food()
+	{
+		Image image = LoadImage("Assets/Textures/food.png");
+		texture = LoadTextureFromImage(image);
+		UnloadImage(image);
+		position = GenerateRandomPos();
+	}
+
+	~Food()
+	{
+		UnloadTexture(texture);
+	}
+
+	Vector2 GenerateRandomPos()
+	{
+		float x = GetRandomValue(0, cellCount - 1);
+		float y = GetRandomValue(0, cellCount - 1);
+		return Vector2{ x, y };
+	}
+
+	void Draw()
+	{
+		DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE);
+	}
+
+};
+
+
 int main() {
-	const int WINDOW_WIDTH = 750;
-	const int WINDOW_HEIGHT = 750;
+	//const int WINDOW_WIDTH = 750;
+	//const int WINDOW_HEIGHT = 750;
 
 
-	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "RetroSnake");
+	InitWindow(cellSize* cellCount, cellSize * cellCount, "RetroSnake");
 	SetTargetFPS(60);
+
+	Food food = Food();
 
 	while (!WindowShouldClose())
 	{
@@ -18,6 +58,7 @@ int main() {
 		
 		//drawing
 		ClearBackground(green);
+		food.Draw();
 		
 		EndDrawing();
 
