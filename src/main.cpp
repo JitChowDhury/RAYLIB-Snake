744c0b4e6650b66351db784227ffb25cdfb77136
JitChowDhury <jitchowdhury8717@gmail.com>
2025-10-03 21:34:50 +0530

    Make the snake grow longer

--- src/main.cpp
+++ src/main.cpp
@@ -40,6 +40,7 @@ class Snake
 public:
 	std::deque<Vector2> body = { Vector2{6,9},Vector2{5,9},Vector2{4,9} };
 	Vector2 direction = { 1,0 };
+	bool addSegment = false;
 
 	void Draw()
 	{
@@ -56,8 +57,16 @@ public:
 
 	void Update()
 	{
+		body.push_front(Vector2Add(body[0], direction));
+
+		if (addSegment == true)
+		{
+			addSegment = false;
+		}
+		else
+		{
 		body.pop_back();
-		body.push_front(Vector2Add(body[0] ,direction));
+		}
 	}
 };
 
@@ -131,6 +140,7 @@ public:
 		if (Vector2Equals(snake.body[0], food.position))
 		{
 			food.position = food.GenerateRandomPos(snake.body);
+			snake.addSegment = true;
 		}
 	}
 };
